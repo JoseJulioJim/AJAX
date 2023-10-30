422baccc97b62adcd5bd295f21c4c1654e1ada68
JoseJulioJim <cdmx2115@amerike.edu.mx>
2023-10-29 21:16:51 -0600

    style añadido

--- AJAX.js
+++ AJAX.js
@@ -11,9 +11,9 @@ async function obtenerDatosLocales() {
   data.Materias.forEach(function (el) {
     Materias += `
       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="card" style="width: 18rem;">          
+        <div class="card" style="width: 36rem;">          
           <div class="card-body">
-            <h5 class="card-title">${el.Materia}</h5>
+            <h2 class="card-title">${el.Materia}</h2>
             <p class="card-text">${el.Docente}</p>
             <p class="card-text">${el.descripcion}</p>
           </div>
---
+++ README.md
@@ -0,0 +1,7 @@
+# José Julio Jiménez del Valle
+
+Parcial 3 - Práctica 6
+
+[GitHubPages](https://josejuliojim.github.io/AJAX/)
+
+[JavaScript](AJAX.js)

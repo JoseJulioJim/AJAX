2228244806c5a13ba597700741a4bb72dbc6d732
JoseJulioJim <cdmx2115@amerike.edu.mx>
2023-10-29 21:07:47 -0600

    m

--- AJAX.js
+++ AJAX.js
@@ -5,16 +5,16 @@ async function obtenerDatosLocales() {
   console.log(data);
 
 
-  let $mats = document.querySelector("#info-shida");
-  let materiasYeahHTML = "";
+  let $subject = document.querySelector("#info-shida");
+  let Materias = "";
 
   data.Materias.forEach(function (el) {
-    materiasYeahHTML += `
+    Materias += `
       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card" style="width: 18rem;">          
           <div class="card-body">
-            <h5 class="card-title">${el.nombre_Materia}</h5>
-            <p class="card-text">${el.nombre_del_Docente}</p>
+            <h5 class="card-title">${el.Materia}</h5>
+            <p class="card-text">${el.Docente}</p>
             <p class="card-text">${el.descripcion}</p>
           </div>
         </div>
@@ -22,7 +22,7 @@ async function obtenerDatosLocales() {
     `;
   });
 
-  $mats.innerHTML = materiasYeahHTML;
+  $subject.innerHTML = Materias;
 }
 
 obtenerDatosLocales();
